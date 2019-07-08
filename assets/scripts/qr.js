86f7c1a03cef777d292047810f76248e71fbd687
Reniel Mallari <renielmallari@Reniels-iMac.local>
2019-07-09 01:06:43 +0800

    testing the console

--- assets/scripts/qr.js
+++ assets/scripts/qr.js
@@ -8,10 +8,11 @@ function openQRCamera(node) {
         alert("No QR code found. Please make sure the QR code is within the camera's frame and try again.");
       } else {
         node.parentNode.previousElementSibling.value = res;
+	   console.log("1:"+ res);
       }
     };
     qrcode.decode(reader.result);
-    console.log(reader.result);
   };
   reader.readAsDataURL(node.files[0]);
+  console.log("2:"+ node.files[0]);
  }
