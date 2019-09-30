7b534f78e885bb096c9ebcc012645ed5fcbabc4a
Reniel Mallari <reinmallari10@gmail.com>
2019-09-30 18:48:18 +0800

    testgsxG

--- application/controllers/Chart.php
+++ application/controllers/Chart.php
@@ -2,17 +2,5 @@
 class Chart extends CI_Controller{
     function __construct(){
       parent::__construct();
-      //load chart_model from model
-      $this->load->model('chart_model');
-    }
-
-    function index(){
-      $this->load->model('Chart_model');
-      $this->load->view('chart_view');
-    }
-
-    public function get_all_data(){
-		$data = $this->chart_model->get_data();
-		echo json_encode($data);
     }
 }
